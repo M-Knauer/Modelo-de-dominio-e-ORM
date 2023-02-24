32e5ae301561159e8089f5e9b185ffe3515ae678
unknown <marcelocrisknauer@gmail.com>
2023-02-24 12:11:12 -0300

    Categoria

--- src/main/java/com/evento/entities/Atividade.java
+++ src/main/java/com/evento/entities/Atividade.java
@@ -10,7 +10,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -33,15 +35,20 @@ public class Atividade {
 	@OneToMany(mappedBy = "atividade")
 	List<Bloco> blocos = new ArrayList<>();
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
 	public Atividade() {
 		
 	}
 
-	public Atividade(Long id, String nome, String descricao, Double preco) {
+	public Atividade(Long id, String nome, String descricao, Double preco, Categoria categoria) {
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
 		this.preco = preco;
+		this.categoria = categoria;
 	}
 
 	public Long getId() {
@@ -83,5 +90,13 @@ public class Atividade {
 	public List<Bloco> getBlocos() {
 		return blocos;
 	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}	
 	
 }
